e2720b3ea405718e38ae626c73351c9f6a75c149
reddyse <praneeth_reddy@outlook.com>
2022-11-09 10:45:58 -0800

    fixed bug on POST handler

--- index.js
+++ index.js
@@ -17,17 +17,3 @@ app.get("/status",(req,res)=>{
 	res.send({"status":"ok"});
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
-app.post("/otp/check", function(req, res) {
-  const user = req.body.user;
-  const code = req.body.otp;
-
-  res.send({
-    'user': user,
-    'code': code,
-	'valid': true
-  });
-});
